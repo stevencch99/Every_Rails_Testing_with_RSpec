aecae646863af7ecd2f6c1dce0e9e35753e0664c
Nine_Bro <shinjisrg@gmail.com>
2018-07-17 00:03:10 +0800

    Omit repeated parts of code in spec/factories/projects.rb

--- spec/factories/projects.rb
+++ spec/factories/projects.rb
@@ -4,26 +4,22 @@ FactoryBot.define do
     description "Sample project for testing purposes"
     due_on 1.week.from_now
     association :owner # 和 owner 產生關聯
-  end
 
-  factory :project_due_yesterday, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on 1.day.ago
-    association :owner
-  end
+    factory :project_due_yesterday do
+      # Omit repeated part of code:
+      # factory :project_due_yesterday, class: Project do
+      # sequence(:name) { |n| "Test Project #{n}" }
+      # description "Sample project for testing purposes"
+      due_on 1.day.ago
+      # association :owner
+    end
 
-  factory :project_due_today, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on Date.current
-    association :owner
-  end
+    factory :project_due_today do
+      due_on Date.current.in_time_zone
+    end
 
-  factory :project_due_tomorrow, class: Project do
-    sequence(:name) { |n| "Test Project #{n}" }
-    description "Sample project for testing purposes"
-    due_on 1.day.from_now
-    association :owner
+    factory :project_due_tomorrow do
+      due_on 1.day.from_now
+    end
   end
 end
